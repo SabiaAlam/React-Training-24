c61a9b11d5c788a792d62becb957b1c623bd3efe
Sabia Alam <119663333+SabiaAlam@users.noreply.github.com>
2024-06-10 22:39:16 +0530

    Add files via upload

---
+++ updated_script.js
@@ -0,0 +1,23 @@
+
+function takevalue() {
+    var fullname = document.getElementById("fullname").value;
+    console.log("Your Name: = " + fullname);
+
+    var username = document.getElementById("username").value;
+    console.log("Your Username := " + username);
+
+    var password = document.getElementById("password").value;
+    console.log("Your Password : = " + password);
+
+    var confirmPassword = document.getElementById("confirm-password").value;
+    console.log("Your Confirm-Password : = " + confirmPassword);
+
+    var email = document.getElementById("email").value;
+    console.log("Your E-mail := " + email);
+
+    var phone = document.getElementById("phone").value;
+    console.log("Your Phone Number is : = " + phone);
+
+    var dob = document.getElementById("dob").value;
+    console.log("Your Date Of Birth = " + dob);
+}
